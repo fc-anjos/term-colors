eae31d10a1f4a9f8e223a9904b1dfd61195e3c6e
fc-anjos <felipe.anjos@usp.br>
2020-05-05 12:06:07 -0300

    Add functionality to brighten colors

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 $(init);
 
+var n_of_colors = 8
+
 
 scale = chroma.scale(['#fafa6e','#2A4858'])
-    .mode('lch').colors(7);
+    .mode('lch').colors(n_of_colors);
 
-console.log(scale);
+term_colors = []
+
+double_colors = n_of_colors*2
+for (var i = 0; i < double_colors; i++) {
+  if (i < 8) {
+    term_colors.push(scale[i]);
+  } else {
+    term_colors.push(chroma(scale[i-8]).darken(-1).saturate(1).hex())
+    }
+}
 
 function init() {
-  for (var i = 0; i < 7; i++) {
-    class_selector = '.color'+i;
-    $(class_selector).css("backgroundColor", scale[i]);
+  for (var i = 0; i < term_colors.length; i++) {
+    color_class = 'color'+i;
+    var element=$(`<div class="${color_class} boxes"></div>`);
+    $('.container').append(element);
+    $('.' + color_class).css("backgroundColor", term_colors[i]);
   }
 }
